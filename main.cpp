28d07ce093e9baea41135c195a84a8e252fae559
takahashi-yukito-0209 <k024g0011@g.neec.ac.jp>
2025-04-23 14:52:31 +0900

    スワップチェーンの作成

--- main.cpp
+++ main.cpp
@@ -104,8 +104,6 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
     // main関数始まってすぐに登録すると良い
     SetUnhandledExceptionFilter(ExportDump);
 
-
-
     // log出力用のフォルダ「logs」の作成
     std::filesystem::create_directory("logs");
 
@@ -220,25 +218,38 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
     assert(device != nullptr);
     Log(logStream, "Complete create D3D12Device!!!\n"); // 初期化完了のログを出す
 
-    //コマンドキューを生成する
+    // コマンドキューを生成する
     ID3D12CommandQueue* commandQueue = nullptr;
     D3D12_COMMAND_QUEUE_DESC commandQueueDesc {};
     hr = device->CreateCommandQueue(&commandQueueDesc, IID_PPV_ARGS(&commandQueue));
 
-    //コマンドキューの生成がうまくいかなかったので起動できない
+    // コマンドキューの生成がうまくいかなかったので起動できない
     assert(SUCCEEDED(hr));
 
-    //コマンドアロケータを生成する
+    // コマンドアロケータを生成する
     ID3D12CommandAllocator* commandAllocator = nullptr;
     hr = device->CreateCommandAllocator(D3D12_COMMAND_LIST_TYPE_DIRECT, IID_PPV_ARGS(&commandAllocator));
-    //コマンドアロケータの生成がうまくいかなかったので起動できない
+    // コマンドアロケータの生成がうまくいかなかったので起動できない
     assert(SUCCEEDED(hr));
 
-    //コマンドリストを生成する
+    // コマンドリストを生成する
     ID3D12GraphicsCommandList* commandList = nullptr;
     hr = device->CreateCommandList(0, D3D12_COMMAND_LIST_TYPE_DIRECT, commandAllocator, nullptr, IID_PPV_ARGS(&commandList));
-    //コマンドリストの生成がうまくいかなかったので起動できない
+    // コマンドリストの生成がうまくいかなかったので起動できない
     assert(SUCCEEDED(hr));
+
+    // スワップチェーンを生成する
+    IDXGISwapChain4* swapChain = nullptr;
+    DXGI_SWAP_CHAIN_DESC1 swapChainDesc {};
+    swapChainDesc.Width = kClientWidth; // 画面の幅。ウィンドウのクライアント領域を同じものにしておく
+    swapChainDesc.Height = kClientHeight; // 画面の高さ。ウィンドウのクライアント領域を同じものにしておく
+    swapChainDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM; // 色の形式
+    swapChainDesc.SampleDesc.Count = 1; // マルチサンプルしない
+    swapChainDesc.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT; // 描画のターゲットとして利用する
+    swapChainDesc.BufferCount = 2; // ダブルバッファ
+    swapChainDesc.SwapEffect = DXGI_SWAP_EFFECT_FLIP_DISCARD; // モニタに移したら、中身を破棄
+    // コマンドキュー、ウィンドウハンドル、設定を渡して生成する
+    hr = dxgiFactory->CreateSwapChainForHwnd(commandQueue, hwnd, &swapChainDesc, nullptr, nullptr, reinterpret_cast<IDXGISwapChain1**>(&swapChain));
 
     MSG msg {};
     // ウィンドウのxボタンが押されるまでループ
