aec0cebb024ef9408c1568f082ce7e6620096590
takahashi-yukito-0209 <k024g0011@g.neec.ac.jp>
2025-04-23 15:45:57 +0900

    画面の色変更完了

--- main.cpp
+++ main.cpp
@@ -252,6 +252,40 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
     hr = dxgiFactory->CreateSwapChainForHwnd(commandQueue, hwnd, &swapChainDesc, nullptr, nullptr, reinterpret_cast<IDXGISwapChain1**>(&swapChain));
     assert(SUCCEEDED(hr));
 
+    // ディスクリプタヒープの生成
+    ID3D12DescriptorHeap* rtvDescriptorHeap = nullptr;
+    D3D12_DESCRIPTOR_HEAP_DESC rtvDescriptorHeapDesc {};
+    rtvDescriptorHeapDesc.Type = D3D12_DESCRIPTOR_HEAP_TYPE_RTV; // レンダーターゲットビュー用
+    rtvDescriptorHeapDesc.NumDescriptors = 2; // ダブルバッファ用に二つ。多くても別に構わない
+    hr = device->CreateDescriptorHeap(&rtvDescriptorHeapDesc, IID_PPV_ARGS(&rtvDescriptorHeap));
+    // ディスクリプタヒープが作れなかったので起動できない
+    assert(SUCCEEDED(hr));
+
+    // SwapChainからResourceを引っ張ってくる
+    ID3D12Resource* swapChainResources[2] = { nullptr };
+    hr = swapChain->GetBuffer(0, IID_PPV_ARGS(&swapChainResources[0]));
+    // うまく取得できなければ起動できない
+    assert(SUCCEEDED(hr));
+    hr = swapChain->GetBuffer(1, IID_PPV_ARGS(&swapChainResources[1]));
+    assert(SUCCEEDED(hr));
+
+    // RTVの設定
+    D3D12_RENDER_TARGET_VIEW_DESC rtvDesc {};
+    rtvDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM_SRGB;
+    rtvDesc.ViewDimension = D3D12_RTV_DIMENSION_TEXTURE2D;
+    // ディスクリプタの先頭を取得する
+    D3D12_CPU_DESCRIPTOR_HANDLE rtvStartHandle = rtvDescriptorHeap->GetCPUDescriptorHandleForHeapStart();
+    // RTVを二つ作るのでディスクリプタを二つ用意
+    D3D12_CPU_DESCRIPTOR_HANDLE rtvHandles[2];
+    // まずひとつ目を作る。一つ目は最初のところに作る。作る場所をこちらで指定してあげる必要がある
+    rtvHandles[0] = rtvStartHandle;
+    device->CreateRenderTargetView(swapChainResources[0], &rtvDesc, rtvHandles[0]);
+    // 二つ目のディスクリプタハンドルを得る(自力で)
+    rtvHandles[1].ptr = rtvHandles[0].ptr + device->GetDescriptorHandleIncrementSize(D3D12_DESCRIPTOR_HEAP_TYPE_RTV);
+    // 二つ目を作る
+    device->CreateRenderTargetView(swapChainResources[1], &rtvDesc, rtvHandles[1]);
+
+
     MSG msg {};
     // ウィンドウのxボタンが押されるまでループ
     while (msg.message != WM_QUIT) {
@@ -261,6 +295,32 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
             DispatchMessage(&msg);
         } else {
             // ゲームの処理
+
+            //画面のクリア処理
+
+            //これから書き込むバックバッファのインデックスを取得
+            UINT backBufferIndex = swapChain->GetCurrentBackBufferIndex();
+
+            //描画先のRTVを設定する
+            commandList->OMSetRenderTargets(1, &rtvHandles[backBufferIndex], false, nullptr);
+            //指定した色で画面全体をクリアする
+            float clearColor[] = { 0.1f, 0.25f, 0.5f, 1.0f };
+            commandList->ClearRenderTargetView(rtvHandles[backBufferIndex], clearColor, 0, nullptr);
+            //コマンドリストの内容を確定させる。すべてぼコマンドを積んでからCloseすること
+            hr = commandList->Close();
+            assert(SUCCEEDED(hr));
+
+            //GPUにコマンドリストの実行を行わせる
+            ID3D12CommandList* commandLists[] = { commandList };
+            commandQueue->ExecuteCommandLists(1, commandLists);
+            //GPUとOSに画面の交換を行うよう通知する
+            swapChain->Present(1, 0);
+            //次のフレーム用のコマンドリストを準備
+            hr = commandAllocator->Reset();
+            assert(SUCCEEDED(hr));
+            hr = commandList->Reset(commandAllocator, nullptr);
+            assert(SUCCEEDED(hr));
+
         }
     }
 
