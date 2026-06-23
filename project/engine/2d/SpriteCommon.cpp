3ccd576661b03fef23ff53d24d7be8a2a7c847be
takahashi-yukito-0209 <k024g0011@g.neec.ac.jp>
2026-06-24 02:44:56 +0900

    2DスプライトでDepthWriteが有効になっていた部分を修正

--- project/engine/2d/SpriteCommon.cpp
+++ project/engine/2d/SpriteCommon.cpp
@@ -370,7 +370,7 @@ void SpriteCommon::CreateGraphicsPipeline(BlendMode mode)
     // Depthの機能を有効化する
     depthStencilDesc.DepthEnable = true;
     // 書き込みします
-    depthStencilDesc.DepthWriteMask = D3D12_DEPTH_WRITE_MASK_ALL;
+    depthStencilDesc.DepthWriteMask = D3D12_DEPTH_WRITE_MASK_ZERO;
     // 比較関数はLessEqual。つまり、近ければ描画される
     depthStencilDesc.DepthFunc = D3D12_COMPARISON_FUNC_LESS_EQUAL;
 
